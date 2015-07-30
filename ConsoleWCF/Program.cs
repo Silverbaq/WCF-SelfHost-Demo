1f4804ebed0db2faca8e60519ceda9a4c6c1047d
Silverbaq <feber.sm@gmail.com>
2015-07-30 14:22:04 +0200

    Added solution

---
+++ ConsoleWCF/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new System.ServiceModel.ServiceHost(typeof(ConsoleWCF.Service1));
+            host.Open();
+
+            Console.WriteLine("Service is running");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SelfHostServiceDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHostServiceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            ServiceHost studentServiceHost = null;
+            try
+            {
+                ////Base Address for Service
+                //Uri httpBaseAddress = new Uri("http://localhost:54321/Hello");
+
+                ////Instantiate ServiceHost
+                //studentServiceHost = new System.ServiceModel.ServiceHost(typeof(SelfHostServiceDemo.Service1),httpBaseAddress);
+
+                ////Add Endpoint to Host
+                //studentServiceHost.AddServiceEndpoint(typeof(IService1), new WSHttpBinding(), "");
+
+                ////Metadata Exchange
+                //ServiceMetadataBehavior serviceBehavior = new ServiceMetadataBehavior();
+                //serviceBehavior.HttpGetEnabled = true;
+                //studentServiceHost.Description.Behaviors.Add(serviceBehavior);
+
+                //Open
+                studentServiceHost.Open();
+                Console.WriteLine("Service is live now at: { 0}", "");
+                Console.ReadKey();
+            }
+
+            catch (Exception ex)
+            {
+                studentServiceHost = null;
+                Console.WriteLine("There is an issue with StudentService" +ex.Message);
+            }
+
+        }
+    }
+}
